81445b0ee92e411941496939cb74ee5073f8d59b
zamadaouf <zamadaouf@gmail.com>
2020-03-12 14:37:31 +0100

    Updates

---
+++ precache-manifest.f5ec57324b8bb8a71842443290545fce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0d8aaa52bfb529a11e0a79eb35d7139",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "fb796a288152db62fb19",
+    "url": "/monsters/static/css/main.1a9920e4.chunk.css"
+  },
+  {
+    "revision": "35da6d0704e617cdc72c",
+    "url": "/monsters/static/js/2.61669658.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters/static/js/2.61669658.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb796a288152db62fb19",
+    "url": "/monsters/static/js/main.50ba89e3.chunk.js"
+  },
+  {
+    "revision": "cac781cbab60b47ad5e0",
+    "url": "/monsters/static/js/runtime-main.5067c463.js"
+  }
+]);
